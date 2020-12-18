a6da4d1920166c9aeaf9adde3de065c810678c73
Alyssa Bull <alyssabull@gmail.com>
2020-12-18 16:35:37 -0700

    Add title info to MovieDetails

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -18,13 +18,40 @@ class MovieDetails extends Component {
     .catch(error => this.setState({ error: error.message}))
   }
 
+  formatGenres = () => {
+    if (!this.state.singleMovie.genres) {
+      return <p className="unavailable-genres">Unavailable</p>
+    } else if (this.state.singleMovie.genres.length > 1) {
+      let commaList = this.state.singleMovie.genres.map(genre => {
+        return(
+          <p className="genre">{genre}</p>
+        )
+      })
+      return commaList;
+    } else {
+      return <p className="genre">{this.state.singleMovie.genres[0]}</p>;
+    }
+  }
+
   render() {
     return(
       <section className="movie-details">
         <section className="banner-container">
           <img src={this.state.singleMovie.backdrop_path} className="banner-img"/>
         </section>
-        <img src={this.state.singleMovie.poster_path} className="poster-img" />
+        <section className="movie-info">
+          <section className="movie-aside">
+            <img src={this.state.singleMovie.poster_path} className="poster-img" />
+          </section>
+          <section className="movie-main">
+            <h1>{this.state.singleMovie.title}</h1>
+            <p>{this.state.singleMovie.tagline}</p>
+            <p>{this.state.singleMovie.average_rating}</p>
+            <section className="genre-list">
+              {this.formatGenres()}
+            </section>
+          </section>
+        </section>
         <MovieTrailers id={this.props.id} />
       </section>
     )

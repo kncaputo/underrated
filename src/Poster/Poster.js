99fd28baf8cd0ee1ce1e2e408562521ea3d0a27c
Alyssa Bull <alyssabull@gmail.com>
2020-12-18 14:56:28 -0700

    Add stars to poster ratings

--- src/Poster/Poster.js
+++ src/Poster/Poster.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './Poster.css';
+import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs';
 
 const Poster = ({ id, title, rating, image }) => {
+  const ratingLevel = () => {
+    if (rating < 5) {
+      return <BsStar />
+    } else if (rating >= 5 && rating < 7) {
+      return <BsStarHalf />
+    } else if (rating >= 7) {
+      return <BsStarFill />
+  }
+}
+
   return(
     <section className='poster' id={id}>
       <img className='card-img' src={image} alt={title} />
       <p className='poster-title'>{title}</p>
-      <p className='poster-rating'>Average Rating: {rating.toFixed(1)} / 10</p>
-      <p className='poster-rating-mini'>{rating.toFixed(1)} / 10</p>
+      <p className='poster-rating'>{ratingLevel()} {(rating*10).toFixed(0)}%</p>
+      <p className='poster-rating-mini'>{(rating*10).toFixed(0)}%</p>
     </section>
   )
 }

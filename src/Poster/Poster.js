f2e640223f88934bbfc991a6c4350a2f2de20e78
Alyssa Bull <alyssabull@gmail.com>
2020-12-18 17:14:22 -0700

    Install react router dom

--- src/Poster/Poster.js
+++ src/Poster/Poster.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import './Poster.css';
-import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 
 const Poster = ({ id, title, rating, image }) => {
-  const ratingLevel = () => {
-    if (rating < 5) {
-      return <BsStar />
-    } else if (rating >= 5 && rating < 7) {
-      return <BsStarHalf />
-    } else if (rating >= 7) {
-      return <BsStarFill />
-  }
-}
 
   return(
     <Link to={`/movie/${id}`}>
       <section className='poster' id={id}>
         <img className='card-img' src={image} alt={title} />
         <p className='poster-title'>{title}</p>
-        <p className='poster-rating'>{ratingLevel()} {(rating*10).toFixed(0)}%</p>
+        <p className='poster-rating'>{(rating*10).toFixed(0)}%</p>
         <p className='poster-rating-mini'>{(rating*10).toFixed(0)}%</p>
       </section>
     </Link>

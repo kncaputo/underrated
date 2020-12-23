f96b11af910f7526822439ad547bb602eb9612dc
Alyssa Bull <alyssabull@gmail.com>
2020-12-23 16:02:42 -0700

    Create Login component

---
+++ src/Login/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Login.css';
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      
+    }
+  }
+}
+
+export default Login;

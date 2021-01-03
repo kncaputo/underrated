82af080654b28158d0a3308e8f31189e56bf6d12
Kara Caputo <kncaputo@gmail.com>
2021-01-03 13:02:05 -0500

    Add tests for StarRating render

--- src/StarRating/StarRating.test.js
+++ src/StarRating/StarRating.test.js
@@ -5,7 +5,7 @@ import '@testing-library/jest-dom';
 import { currentUser } from '../testData';
 
 describe('StarRating', () => {
-  it('should render correctly', () => {
+  it('should render correctly when there\'s a user with a star rating', () => {
     render(
       <StarRating
         currentUser={currentUser}
@@ -16,6 +16,36 @@ describe('StarRating', () => {
     )
 
     expect(screen.getByText('Your rating is 5')).toBeInTheDocument();
+    expect(screen.getByRole('status')).toBeInTheDocument();
+  })
+
+  it('should render correctly when there\'s a user without a star rating', () => {
+    render(
+      <StarRating
+        currentUser={currentUser}
+        currentUserRating={0}
+        canEdit={!currentUser ? false : true} 
+        setStarRating={jest.fn()}
+      />
+    )
+
+    expect(screen.getByText('Rate this movie')).toBeInTheDocument();
+    expect(screen.getByRole('status')).toBeInTheDocument();
+  })
+
+  it('should render correctly when there\'s no user', () => {
+    let currentUser1 = null;
+
+    render(
+      <StarRating
+        currentUser={currentUser1}
+        currentUserRating={0}
+        canEdit={!currentUser1 ? false : true} 
+        setStarRating={jest.fn()}
+      />
+    )
+
+    expect(screen.getByText('Please log in to rate')).toBeInTheDocument();
     expect(screen.getByRole('status')).toBeInTheDocument();
   })
 })

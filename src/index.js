0a4b0a0be93b8e91bebc709dbd7e1ff558e58a63
Kara Caputo <kncaputo@gmail.com>
2021-01-19 15:43:47 -0500

    Add basename to app

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App/App';
 
-const router = <BrowserRouter><App /></BrowserRouter>
+const router = <BrowserRouter basename='/underrated'><App /></BrowserRouter>
 
 ReactDOM.render(
  router,

9697b5b9dd8c9070c5100a277347f41bdf5dc2c5
Kara Caputo <kncaputo@gmail.com>
2021-01-21 10:57:50 -0500

    Add helper functions to generate genre and rating buttons

--- src/App/App.js
+++ src/App/App.js
@@ -94,6 +94,23 @@ class App extends Component {
     this.setState({ input: '', dropdownValue: 'all', ratingValue: 'any' })
   }
 
+  generateGenreButtons = () => {
+    const genres = ['Adventure', 'Animation', 'Comdey', 'Crime', 'Drama', 'Family', 'Fantasy', 'History', 'Horror', 'Music', 'Romance', 'Science', 'Thriller', 'War', 'Western'];
+    
+    return genres.map(genre => {
+      return <button name={genre} onClick={(event) => this.handleDropdownValue(event)}>{genre.toLowerCase()}</button>
+    })
+  }
+
+  generateRatingsButtons = () => {
+    const ratings = [{name: 'one', value: 1}, {name: 'two', value: 2}, {name: 'three', value: 3}, {name: 'four', value: 4}, {name: 'five', value: 5}, 
+    {name: 'six', value: 6}, {name: 'seven', value: 7}, {name: 'eight', value: 8}, {name: 'nine', value: 9}, {name: 'ten', value: 10}];
+    
+    return ratings.map(rating => {
+      return  <button name={rating.name} value={rating.value} onClick={(event) => this.handleRatingValue(event)}>{rating.name}</button>
+    })
+  }
+
   render() {
     return(
       <main>
@@ -141,38 +158,13 @@ class App extends Component {
                     title=''
                     className='genre-dropdown'>
                     <button onClick={() => this.setState({ dropdownValue: 'all' })}>all</button>
-                    <button name="Action" onClick={(event) => this.handleDropdownValue(event)}>action</button>
-                    <button name="Adventure" onClick={(event) => this.handleDropdownValue(event)}>adventure</button>
-                    <button name="Animation" onClick={(event) => this.handleDropdownValue(event)}>animation</button>
-                    <button name="Comedy" onClick={(event) => this.handleDropdownValue(event)}>comedy</button>
-                    <button name="Crime" onClick={(event) => this.handleDropdownValue(event)}>crime</button>
-                    <button name="Drama" onClick={(event) => this.handleDropdownValue(event)}>drama</button>
-                    <button name="Family" onClick={(event) => this.handleDropdownValue(event)}>family</button>
-                    <button name="Fantasy" onClick={(event) => this.handleDropdownValue(event)}>fantasy</button>
-                    <button name="History" onClick={(event) => this.handleDropdownValue(event)}>history</button>
-                    <button name="Horror" onClick={(event) => this.handleDropdownValue(event)}>horror</button>
-                    <button name="Music" onClick={(event) => this.handleDropdownValue(event)}>music</button>
-                    <button name="Romance" onClick={(event) => this.handleDropdownValue(event)}>romance</button>
-                    <button name="Science fiction" onClick={(event) => this.handleDropdownValue(event)}>science fiction</button>
-                    <button name="Thriller" onClick={(event) => this.handleDropdownValue(event)}>thriller</button>
-                    <button name="War" onClick={(event) => this.handleDropdownValue(event)}>war</button>
-                    <button name="Western" onClick={(event) => this.handleDropdownValue(event)}>western</button>
+                    {this.generateGenreButtons()}
                   </DropdownButton>
                   <p className="dropdown-text">with <span className="active-text">{this.state.ratingValue}</span> rating</p>
                   <DropdownButton
                     title=''
                     className='rating-dropdown'>
-                    <button onClick={() => this.setState({ ratingValue: 'any' })}>any</button>
-                    <button name="one" value="1" onClick={(event) => this.handleRatingValue(event)}>one</button>
-                    <button name="two" value="2" onClick={(event) => this.handleRatingValue(event)}>two</button>
-                    <button name="three" value="3" onClick={(event) => this.handleRatingValue(event)}>three</button>
-                    <button name="four" value="4" onClick={(event) => this.handleRatingValue(event)}>four</button>
-                    <button name="five" value="5" onClick={(event) => this.handleRatingValue(event)}>five</button>
-                    <button name="six" value="6" onClick={(event) => this.handleRatingValue(event)}>six</button>
-                    <button name="seven" value="7" onClick={(event) => this.handleRatingValue(event)}>seven</button>
-                    <button name="eight" value="8" onClick={(event) => this.handleRatingValue(event)}>eight</button>
-                    <button name="nine" value="9" onClick={(event) => this.handleRatingValue(event)}>nine</button>
-                    <button name="ten" value="10" onClick={(event) => this.handleRatingValue(event)}>ten</button>
+                    {this.generateRatingsButtons()}
                   </DropdownButton>
                   {this.state.input !== '' || this.state.dropdownValue !== 'all' || this.state.ratingValue !== 'any' &&
                     <button className="clear" onClick={() => this.clearFilters()}>clear</button>

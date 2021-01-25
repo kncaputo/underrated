77be47ef23a7f3e0dd3af7f2b90f27a0e0f0d4ec
Kara Caputo <kncaputo@gmail.com>
2021-01-25 16:04:40 -0500

    Add styles to Watchlist

--- src/Watchlist/Watchlist.js
+++ src/Watchlist/Watchlist.js
@@ -3,9 +3,8 @@ import './Watchlist.scss';
 
 const Watchlist = () => {
   return(
-    <section>
+    <section id='watchlist-main'>
       <h2 id='section-title'>Coming Soon</h2>
-      <button>Click Me</button>
     </section>
   )
 }

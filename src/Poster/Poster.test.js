0a914e1dcf85f9019ff9187af2bf9481df956c06
Kara Caputo <kncaputo@gmail.com>
2021-03-23 11:01:01 -0400

    Complete Poster test

--- src/Poster/Poster.test.js
+++ src/Poster/Poster.test.js
@@ -1,1 +1,32 @@
-// test that the poster is rendered
+import { render } from 'react-dom'
+import Poster from './Poster'
+import { mockMovies } from '../testData'
+import { BrowserRouter } from 'react-router-dom'
+import { act } from "react-dom/test-utils";
+
+describe('Poster', () => {
+  let container = null
+
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  })
+  
+  it('should render a poster', () => {
+      act(() => {
+        render(
+          <BrowserRouter>
+            <Poster
+              id={mockMovies[0].id}
+              title={mockMovies[0].title}
+              rating={mockMovies[0].average_rating}
+              image={mockMovies[0].poster_path}
+              key={mockMovies[0].id}
+            />  
+          </BrowserRouter>, container
+        ) 
+      })
+
+    expect(container.textContent).toBe('Money Plane☆ 6.1/10☆ 6.1/10')
+  })
+})
